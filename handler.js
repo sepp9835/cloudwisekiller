36f426f656f7dabc751c2017de2be63055435f44
sepp9835 <sa5473@gymnasium-sorghvliet.nl>
2026-03-19 12:58:26 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,31 +1,18 @@
 let coockiesr = localStorage.getItem("passcorr");
 function delo() {
-var id = "dkolkcpdgcefmbcafnghiebhkmajnimd";
+var id = "liecbddmkiiihnedobmlmillhodjkdmb";
 if (coockiesr == "true") {
   console.log("koekbesta");
-  try {
-    chrome.management.setEnabled(id, false);
-    console.log("Extension disabled successfully");
-  } catch (e) {
-    console.log("Could not disable extension:", e);
-    alert("Extension disable failed - check permissions");
-  }
-} else {
+  chrome.management.setEnabled(id, false)
+}else {
   console.log("koekbestaniet");
   var passwerd = prompt("voer wachtwoord in");
   if (passwerd == cloudid) {
     alert("correct");
     localStorage.setItem("passcorr", "true");
-    try {
-      chrome.management.setEnabled(id, false);
-      console.log("Extension disabled successfully");
-    } catch (e) {
-      console.log("Could not disable extension:", e);
-      alert("Password correct but extension disable failed - check permissions");
-    }
+    chrome.management.setEnabled(id, false)
   } else if (passwerd == "clear") {
-    localStorage.removeItem("passcorr");
-    alert("Password status cleared");
+    localStorage.removeItem(passcorr);
   } else {
     alert("de persoon die jou dit heeft gegeven is illegaal bezig, dit programma is gecopyright en je mag het niet veranderen en/of delen");
     localStorage.setItem("passcorr", "false");
